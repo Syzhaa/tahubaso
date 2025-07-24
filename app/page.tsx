7b51d8996f1587aaec343fc26c738a4f0b6104fe
Syzhaa <saleh.slh02@gmail.com>
2025-07-25 02:31:28 +0800

    tahubaso

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation';
 
 export default function HomePage() {
   // Langsung arahkan ke halaman menu toko default Anda
-  redirect('/menu/toko-sejahtera-01');
+  redirect('/menu/tahubaso');
 }
